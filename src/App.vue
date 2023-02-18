7111a8252d179691b2af2c8f4e6ad0ddb0a60b43
Wall <ilnazamirhanov61@gmail.com>
2023-02-18 23:52:31 +0300

    add app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,43 @@
-
+<template>
+  <div class="wrapper">
+    <!-- header -->
+    <div class="wrapper-content">
+      <div class="view">
+        <div class="container">
+          <Form @onSubmit="handleSubmit" />
+          <List :items="items" />
+        </div>
+      </div>
+    </div>
+    <!-- footer -->
+  </div>
+</template>
 
 <script>
+import { ref } from 'vue'
+
+import Form from '@/components/Form'
+import List from '@/components/List'
 
 export default {
-  name: 'App',
-  components: {
+  components: { Form, List },
+  setup() {
+    const items = ref([
+      {
+        id: 1,
+        body: 'Tatneft',
+        likes: 55,
+        avatar: `https://avatars.dicebear.com/api/male/1.svg`,
+        date: new Date(Date.now()).toLocaleString()
+      },
+
+    ])
+    const handleSubmit = item => items.value.push(item)
+
+    return {
+      items,
+      handleSubmit
+    }
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
